97aeecf95603fb2a7834636d8276418b8deb96e2
jackecrouse <112016157+jackecrouse@users.noreply.github.com>
2022-10-05 16:24:44 -0400

    Update GeometryBuilder.java

--- src/input/builder/GeometryBuilder.java
+++ src/input/builder/GeometryBuilder.java
@@ -1,9 +1,57 @@
 package input.builder;
 
+import java.util.List;
+
+import input.components.FigureNode;
+import input.components.point.PointNode;
+import input.components.point.PointNodeDatabase;
+import input.components.segment.SegmentNode;
+import input.components.segment.SegmentNodeDatabase;
+
 public class GeometryBuilder extends DefaultBuilder {
 
-	public GeometryBuilder() {
-		// TODO Auto-generated constructor stub
+	
+	public GeometryBuilder() 
+	{
+		
 	}
 
+	@Override
+    public FigureNode buildFigureNode(String description,
+    		                          PointNodeDatabase points,
+    		                          SegmentNodeDatabase segments)
+    {
+        return null;
+    }
+    
+	@Override
+    public SegmentNodeDatabase buildSegmentNodeDatabase()
+    {
+        return null;
+    }
+    
+	@Override
+    public void addSegmentToDatabase(SegmentNodeDatabase segments, PointNode from, PointNode to)
+    {
+    	if (segments != null) segments.addUndirectedEdge(from, to);
+    }
+    
+	@Override
+    public SegmentNode buildSegmentNode(PointNode pt1, PointNode pt2)
+    {
+        return null;
+    }
+    
+	@Override
+    public PointNodeDatabase buildPointDatabaseNode(List<PointNode> points)
+    {
+        return null;
+    }
+    
+	@Override
+    public PointNode buildPointNode(String name, double x, double y)
+    {
+        return null;
+    }
+	
 }
